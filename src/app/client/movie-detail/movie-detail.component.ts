421636120734b01f678d45c671d3e8e9a29cd719
Taka-1311 <ngthanhhung11@outlook.com>
2022-06-19 21:10:59 +0700

    fix movie trailer iframe

--- src/app/client/movie-detail/movie-detail.component.ts
+++ src/app/client/movie-detail/movie-detail.component.ts
@@ -1,7 +1,8 @@
 import { MovieService } from './../../services/movie.service';
 import { Movie } from './../../models/movie';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Sanitizer } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { DomSanitizer, SafeHtml, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-movie-detail',
@@ -10,40 +11,50 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class MovieDetailComponent implements OnInit {
 
-  movie!: Movie
+  movie: Movie = new Movie();
   idDetail!: any
-  trailerId: string = "https://www.youtube.com/embed/"
+  iframeSrc: string = "https://www.youtube.com/embed/"
+  videoUrl: SafeResourceUrl = '';
 
-  constructor(private activateRoute: ActivatedRoute, private movieService: MovieService) { }
+  constructor(private activateRoute: ActivatedRoute, private movieService: MovieService, private santitizer: DomSanitizer) { }
 
   ngOnInit(): void {
-    this.getMovieDetail();
+    this.getMovieDetail()
   }
 
   getMovieDetail() {
     this.activateRoute.queryParams.subscribe((param: any) => {
       this.idDetail = param;
-      console.log(this.idDetail)
-    })
-    this.movieService.getMovieDetail(this.idDetail.id).subscribe({
-      next: (value: any) => {
-        this.movie = value.content;
-      },
-      error: (e) => console.log("GET failed: ", e),
+      this.movieService.getMovieDetail(this.idDetail.id).subscribe({
+        next: (value: any) => {
+          this.movie = value.content;
+          this.readTrailerId(this.movie.trailer)
+        },
+        error: (e) => console.log("GET failed: ", e),
+      })
+
     })
   }
 
-  readTrailerId(string: string) {
+  readTrailerId(string: string): any {
     const isEmbeded: string = "https://www.youtube.com/embed/"
     const isWatch: string = "https://www.youtube.com/watch?v="
+    const isDefault: string = "https://youtu.be/"
 
-    if (string[isEmbeded.length] === '/') {
-      this.trailerId += string.slice(+string[isEmbeded.length + 1], string.length)
-      console.log(this.trailerId)
-    } else if (string[isWatch.length] === '=') {
-      this.trailerId += string.slice(+string[isWatch.length + 1], string.length)
-      console.log(this.trailerId)
+    if (string[isEmbeded.length - 1] === '/') {
+      const videoCode = string.slice(30, (string.length))
+      this.iframeSrc += videoCode
+      this.videoUrl = this.santitizer.bypassSecurityTrustResourceUrl(this.iframeSrc)
+    }
+    else if (string[isWatch.length - 1] === '=') {
+      const videoCode = string.slice(32, (string.length))
+      this.iframeSrc += videoCode
+      this.videoUrl = this.santitizer.bypassSecurityTrustResourceUrl(this.iframeSrc)
+    }
+    else if (string[isDefault.length - 1] === '/') {
+      const videoCode = string.slice(17, (string.length))
+      this.iframeSrc += videoCode
+      this.videoUrl = this.santitizer.bypassSecurityTrustResourceUrl(this.iframeSrc)
     }
   }
-
 }
